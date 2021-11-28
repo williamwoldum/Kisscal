a15e1ea1e054e72207176f45ff141ba76feba04c
William Woldum <41169797+williamwoldum@users.noreply.github.com>
2021-11-28 18:28:52 +0100

    Delete src directory

--- src/test/headers/file_handler/get_cal_suite.h
+++
@@ -1,8 +0,0 @@
-#ifndef GET_CAL_SUITE_H
-#define GET_CAL_SUITE_H
-
-#include "../CuTest.h"
-
-CuSuite* get_suite_get_cal_test();
-
-#endif
--- src/test/headers/include_suites.h
+++
@@ -1,6 +0,0 @@
-#ifndef INCLUDE_SUITES_H
-#define INCLUDE_SUITES_H
-
-#include "../headers/file_handler/get_cal_suite.h"
-
-#endif
