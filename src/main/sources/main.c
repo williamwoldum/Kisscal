a85fd3713c60103cf60eee79b727145b1614287e
mikkelklitlund <mik.kkel@hotmail.com>
2021-12-08 13:48:40 +0100

    Fixed next/previous week

--- src/main/sources/main.c
+++ src/main/sources/main.c
@@ -11,8 +11,7 @@
 #include "../headers/regex_handler.h"
 #include "../headers/time_handler.h"
 
-int main()
-{
+int main() {
     srand(time(NULL));
 
     setup_regex();
@@ -23,10 +22,8 @@ int main()
     prn_cal();
 
     int run = 1;
-    while (run)
-    {
+    while (run) {
         run = prompt_user_input();
-        printf("%d %d %ld\n", get_t_data(current_cal.time, t_week), get_t_data(current_cal.time, t_year), current_cal.time);
     }
 
     return (0);
