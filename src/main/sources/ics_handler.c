cb79fda390aa5727f00c8b9f766a79c9ed803e19
Frihauge <38877854+MathiasFrihauge@users.noreply.github.com>
2021-12-13 14:38:32 +0100

    mappe henter filnavn + diller kode til at fixe mktime

--- src/main/sources/ics_handler.c
+++ src/main/sources/ics_handler.c
@@ -1,3 +1,4 @@
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,8 +13,7 @@ static void create_event(FILE *, calendar, int, int);
 static void create_assignment(FILE *, calendar, int, int);
 static char *get_deadline(assignment);
 
-void convert_cal_to_ics(calendar *cal)
-{
+void convert_cal_to_ics(calendar *cal) {
     FILE *cal_file = fopen("./ics_output.ics", "w");
     fprintf(cal_file,
             "BEGIN:VCALENDAR\n"
@@ -24,18 +24,14 @@ void convert_cal_to_ics(calendar *cal)
 
     int day;
 
-    for (day = 0; day < DAYS_IN_WEEK; day++)
-    {
+    for (day = 0; day < DAYS_IN_WEEK; day++) {
         int hour;
-        for (hour = 0; hour < CONTENT_IN_DAY; hour++)
-        {
-            if (cal->days[day].events[hour].valid == 1)
-            {
+        for (hour = 0; hour < CONTENT_IN_DAY; hour++) {
+            if (cal->days[day].events[hour].valid == 1) {
                 create_event(cal_file, *cal, day, hour);
             }
 
-            if (cal->days[day].assignments[hour].valid == 1)
-            {
+            if (cal->days[day].assignments[hour].valid == 1) {
                 create_assignment(cal_file, *cal, day, hour);
             }
         }
@@ -46,8 +42,7 @@ void convert_cal_to_ics(calendar *cal)
     /*cal.days[0].events[0].*/
 }
 
-static void create_event(FILE *cal_file, calendar cal, int day, int hour)
-{
+static void create_event(FILE *cal_file, calendar cal, int day, int hour) {
     char *dtstart = get_dtstart_dtend(cal.days[day].events[hour], 1);
 
     char *dtend = get_dtstart_dtend(cal.days[day].events[hour], 0);
@@ -70,8 +65,7 @@ static void create_event(FILE *cal_file, calendar cal, int day, int hour)
     fprintf(cal_file, "END:VEVENT\n");
 }
 
-static void create_assignment(FILE *cal_file, calendar cal, int day, int hour)
-{
+static void create_assignment(FILE *cal_file, calendar cal, int day, int hour) {
     char *dtstart = get_deadline(cal.days[day].assignments[hour]);
 
     char *dtend = get_deadline(cal.days[day].assignments[hour]);
@@ -94,18 +88,14 @@ static void create_assignment(FILE *cal_file, calendar cal, int day, int hour)
     fprintf(cal_file, "END:VEVENT\n");
 }
 
-static char *get_dtstart_dtend(event event, int isStart)
-{
+static char *get_dtstart_dtend(event event, int isStart) {
     char *final = (char *)malloc(48 * sizeof(char));
 
-    if (isStart)
-    {
+    if (isStart) {
         time_t start_time = event.start_time;
         struct tm *start = localtime(&start_time);
         sprintf(final, "DTSTART;TZID=Europe/Copenhagen:%02d%02d%02dT%02d%02d%02d", start->tm_year + 1900, start->tm_mon + 1, start->tm_mday, start->tm_hour, start->tm_min, start->tm_sec);
-    }
-    else
-    {
+    } else {
         time_t end_time = event.end_time;
         struct tm *end = localtime(&end_time);
         sprintf(final, "DTEND;TZID=Europe/Copenhagen:%02d%02d%02dT%02d%02d%02d", end->tm_year + 1900, end->tm_mon + 1, end->tm_mday, end->tm_hour, end->tm_min, end->tm_sec);
@@ -114,8 +104,7 @@ static char *get_dtstart_dtend(event event, int isStart)
     return final;
 }
 
-static char *get_deadline(assignment assignment)
-{
+static char *get_deadline(assignment assignment) {
     char *final = (char *)malloc(48 * sizeof(char));
     time_t deadline_time = assignment.deadline;
     struct tm *time = localtime(&deadline_time);
@@ -125,19 +114,30 @@ static char *get_deadline(assignment assignment)
     return final;
 }
 
-static int get_uid()
-{
+static int get_uid() {
     int r = rand() % 1000000000;
     return r;
 }
 
-void import_ics(calendar cal)
-{
-    char *path = "/Users/mathiasfrihauge/Documents/UNI/p1projekt/Kisscal/src/Importfiles/ics_output-input.ics";
+void import_ics(calendar cal) {
+    DIR *folder;
+    struct dirent *dir;
+    char *name;
+    folder = opendir("./Importfiles");
+
+    while ((dir = readdir(folder)) != NULL) {
+        printf("name %s\n", dir->d_name);
+        name = dir->d_name;
+    }
+
+    char path[300];
+
+    sprintf(path, "./Importfiles/%s", name);
+    printf("path %s\n", path);
+    closedir(folder);
 
     FILE *file = fopen(path, "r");
-    if (file == NULL)
-    {
+    if (file == NULL) {
         printf("File not found");
         return;
     }
@@ -146,10 +146,8 @@ void import_ics(calendar cal)
 
     char c;
     int linecount = 0;
-    while ((c = fgetc(file)) != EOF)
-    {
-        if (c == '\n')
-        {
+    while ((c = fgetc(file)) != EOF) {
+        if (c == '\n') {
             linecount++;
         }
     }
@@ -160,18 +158,30 @@ void import_ics(calendar cal)
     time_t start_time = 0;
     time_t end_time = 0;
     char *title;
-    for (i = 0; i < linecount; i++)
-    {
+    struct tm time;
+    int year1 = 0, month1 = 0;
 
+    /*lorte kode til at sætte en standard værdi åbenbart*/
+    sscanf("20211005T123410", "%4d%2d%2dT%2d%2d",
+           &year1,
+           &month1,
+           &time.tm_mday,
+           &time.tm_hour,
+           &time.tm_min);
+    time.tm_year = year1 - 1900;
+    time.tm_mon = month1 - 1;
+    start_time = mktime(&time);
+    /*------------------------------------*/
+
+    time.tm_isdst = 0;
+
+    for (i = 0; i < linecount; i++) {
         fgets(line, 100, file);
 
-        if (strstr(line, "BEGIN:VEVENT"))
-        {
+        if (strstr(line, "BEGIN:VEVENT")) {
             title = (char *)calloc(1, 100);
             eventstatus = 1;
-        }
-        else if (strstr(line, "END:VEVENT"))
-        {
+        } else if (strstr(line, "END:VEVENT")) {
             eventstatus = 0;
             printf("starttime: %ld\n", start_time);
             printf("endtime: %ld\n", end_time);
@@ -183,17 +193,15 @@ void import_ics(calendar cal)
             /* add_event(title, start_time, end_time);*/
         }
 
-        if (eventstatus)
-        {
-            struct tm time;
-            time.tm_isdst = 0;
-            char buffer[100];
-            if (strstr(line, "DTSTART"))
-            {
+        if (eventstatus) {
+            /*time.tm_isdst = 0;*/
 
+            char *buffer = (char *)calloc(1, 100);
+
+            if (strstr(line, "DTSTART")) {
                 sscanf(line, "%*[^:]:%s", buffer);
-                printf("START %s\n", buffer);
-                int year, month;
+
+                int year = 0, month = 0;
                 sscanf(buffer, "%4d%2d%2dT%2d%2d",
                        &year,
                        &month,
@@ -202,13 +210,10 @@ void import_ics(calendar cal)
                        &time.tm_min);
                 time.tm_year = year - 1900;
                 time.tm_mon = month - 1;
-
+                printf("START %s\n", buffer);
                 start_time = mktime(&time);
-                printf("%ld\n", start_time);
-            }
-            else if (strstr(line, "DTEND"))
-            {
-
+                printf("actualtime: %ld\n", start_time);
+            } else if (strstr(line, "DTEND")) {
                 sscanf(line, "%*[^:]:%s", buffer);
                 printf("SLUT %s\n", buffer);
                 int year, month;
@@ -222,10 +227,8 @@ void import_ics(calendar cal)
                 time.tm_mon = month - 1;
 
                 end_time = mktime(&time);
-                printf("%ld\n", end_time);
-            }
-            else if (strstr(line, "SUMMARY"))
-            {
+                printf("actualtime: %ld\n", end_time);
+            } else if (strstr(line, "SUMMARY")) {
                 char buf[100];
                 sscanf(line, "%[^:]:%s", buf, title);
 
