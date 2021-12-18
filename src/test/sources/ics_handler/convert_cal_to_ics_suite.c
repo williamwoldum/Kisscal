28ffec24f4d0bdf6b5c2392145e8b6e2bbbbbcbc
williamwoldum <wilwoldum@gmail.com>
2021-12-19 00:22:16 +0100

    added ics export tests

--- src/test/sources/ics_handler/convert_cal_to_ics_suite.c
+++ src/test/sources/ics_handler/convert_cal_to_ics_suite.c
@@ -1,16 +1,112 @@
 #include "../../headers/ics_handler/convert_cal_to_ics_suite.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 #include "../../../main/headers/datatypes.h"
+#include "../../../main/headers/file_handler.h"
+#include "../../../main/headers/ics_handler.h"
+#include "../../../main/headers/time_handler.h"
 #include "../../headers/CuTest.h"
 
-void get_cal_test_week_5_2021_fresh(CuTest* tc) {
-      CuAssertTrue(tc, 1);
+void convert_cal_to_ics_test_zero_events(CuTest* tc) {
+    prepare_file(0);
+    time_t cal_time = get_cal_time_from_day_time(time(NULL));
+    calendar cal = get_cal(cal_time);
+    convert_cal_to_ics(&cal);
+
+    FILE* expected = fopen("./test/assets/export_test_1.ics", "r");
+    FILE* actual = fopen("./ics_output.ics", "r");
+
+    if (!actual || !expected) {
+        CuFail(tc, "Failed to open ics files");
+    }
+
+    char exp_buffer[100], act_buffer[100];
+    int succes = 1;
+    while (fgets(exp_buffer, sizeof(exp_buffer), expected) && fgets(act_buffer, sizeof(act_buffer), actual)) {
+        if (strcmp(exp_buffer, act_buffer)) {
+            if (!(strstr(exp_buffer, "UID") && strstr(act_buffer, "UID"))) {
+                succes = 0;
+            }
+        }
+    }
+
+    fclose(expected);
+    fclose(actual);
+    prepare_file(0);
+
+    CuAssertTrue(tc, succes);
+}
+
+void convert_cal_to_ics_test_one_event(CuTest* tc) {
+    prepare_file(0);
+    time_t cal_time = get_cal_time_from_day_time(time(NULL));
+    add_event("Party", cal_time, cal_time + SECS_IN_HOUR);
+    calendar cal = get_cal(cal_time);
+    convert_cal_to_ics(&cal);
+
+    FILE* expected = fopen("./test/assets/export_test_2.ics", "r");
+    FILE* actual = fopen("./ics_output.ics", "r");
+
+    if (!actual || !expected) {
+        CuFail(tc, "Failed to open ics files");
+    }
+
+    char exp_buffer[100], act_buffer[100];
+    int succes = 1;
+    while (fgets(exp_buffer, sizeof(exp_buffer), expected) && fgets(act_buffer, sizeof(act_buffer), actual)) {
+        if (strcmp(exp_buffer, act_buffer)) {
+            if (!(strstr(exp_buffer, "UID") && strstr(act_buffer, "UID"))) {
+                succes = 0;
+            }
+        }
+    }
+
+    fclose(expected);
+    fclose(actual);
+    prepare_file(0);
+
+    CuAssertTrue(tc, succes);
+}
+
+void convert_cal_to_ics_test_one_event_and_one_assignment(CuTest* tc) {
+    prepare_file(0);
+    time_t cal_time = get_cal_time_from_day_time(time(NULL));
+    add_event("Movie night", cal_time, cal_time + SECS_IN_HOUR);
+    add_assignemnt("Math test", cal_time + SECS_IN_DAY * 2, 7, 3);
+    calendar cal = get_cal(cal_time);
+    convert_cal_to_ics(&cal);
+
+    FILE* expected = fopen("./test/assets/export_test_3.ics", "r");
+    FILE* actual = fopen("./ics_output.ics", "r");
+
+    if (!actual || !expected) {
+        CuFail(tc, "Failed to open ics files");
+    }
+
+    char exp_buffer[100], act_buffer[100];
+    int succes = 1;
+    while (fgets(exp_buffer, sizeof(exp_buffer), expected) && fgets(act_buffer, sizeof(act_buffer), actual)) {
+        if (strcmp(exp_buffer, act_buffer)) {
+            if (!(strstr(exp_buffer, "UID") && strstr(act_buffer, "UID"))) {
+                succes = 0;
+            }
+        }
+    }
+
+    fclose(expected);
+    fclose(actual);
+    prepare_file(0);
+
+    CuAssertTrue(tc, succes);
 }
 
 CuSuite* get_suite_convert_cal__to_ics_test() {
     CuSuite* suite = CuSuiteNew();
-    SUITE_ADD_TEST(suite, get_cal_test_week_5_2021_fresh);
+    SUITE_ADD_TEST(suite, convert_cal_to_ics_test_zero_events);
+    SUITE_ADD_TEST(suite, convert_cal_to_ics_test_one_event);
+    SUITE_ADD_TEST(suite, convert_cal_to_ics_test_one_event_and_one_assignment);
     return suite;
 }
